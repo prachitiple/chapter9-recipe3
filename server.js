eb7f50c99b77a523e522270dc6ad64bf6b93826a
Shekhar Gulati <shekhargulati84@gmail.com>
2014-05-25 22:15:19 +0530

    used express

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res){
+	res.send('Hello World!!');
+})
 
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-var server = http.createServer(function(req,res){
-	res.writeHead(200, {'Content-Type':'text/plain'});
-	res.end('Hello World!!');
-});
-
-server.listen(port,ip);
+app.listen(port,ip);
 console.log('Server running at http://%s:%d',ip,port);
